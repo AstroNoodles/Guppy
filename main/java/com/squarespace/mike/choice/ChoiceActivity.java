47b85cf27b1895b27d6df92928eea020ecf2e8dc
AstroNoodles <astroelementalmod@gmail.com>
2017-03-28 12:30:33 -0400

    newly update

--- main/java/com/squarespace/mike/choice/ChoiceActivity.java
+++ main/java/com/squarespace/mike/choice/ChoiceActivity.java
@@ -2,6 +2,7 @@ package com.squarespace.mike.choice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ChoiceActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class ChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        Toolbar title = (Toolbar) findViewByID(R.id.toolbar);
+        setActionBar(title);
     }
 }
